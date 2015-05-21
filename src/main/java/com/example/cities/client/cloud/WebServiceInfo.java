3e0ac650dc63b6de962b2d42364c6fb19676350a
amostafa88 <amostafa88@gmail.com>
2015-05-21 19:44:26 +0300

    Initial commit

---
+++ src/main/java/com/example/cities/client/cloud/WebServiceInfo.java
@@ -0,0 +1,10 @@
+package com.example.cities.client.cloud;
+
+import org.springframework.cloud.service.UriBasedServiceInfo;
+
+public class WebServiceInfo extends UriBasedServiceInfo {
+    public WebServiceInfo(String id, String url) {
+    	super(id, "http://cities.mybluemix.net/");
+        //super(id, url);
+    }
+}
---
+++ src/main/java/com/example/cities/client/cloud/localconfig/CitiesWebServiceInfoCreator.java
@@ -0,0 +1,24 @@
+package com.example.cities.client.cloud.localconfig;
+
+import com.example.cities.client.cloud.WebServiceInfo;
+import org.springframework.cloud.localconfig.LocalConfigServiceInfoCreator;
+import org.springframework.cloud.service.UriBasedServiceData;
+
+public class CitiesWebServiceInfoCreator extends LocalConfigServiceInfoCreator<WebServiceInfo> {
+    public static final String CITIES_TAG = "cities";
+
+    public CitiesWebServiceInfoCreator() {
+        super("http");
+    }
+
+    @Override
+    public boolean accept(UriBasedServiceData serviceData) {
+        return super.accept(serviceData) && CITIES_TAG.equals(serviceData.getKey());
+    }
+
+    @Override
+    public WebServiceInfo createServiceInfo(String id, String uri) {
+    	uri="http://cities.mybluemix.net/";
+        return new WebServiceInfo(id, uri);
+    }
+}
